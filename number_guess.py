b583c13c0d6806a69a5d0b0e6f4a3a7c0f14fb5b
SoumenX-CZ <90548394+SoumenX-CZ@users.noreply.github.com>
2023-06-25 08:44:05 +0200

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,52 @@
+import random
+import time
+number=random.randint(1, 200)
+
+def intro():
+    print("Jméno?")
+    name=input() 
+    print(name + ", zahrajeme si hru. Myslím si číslo od 0 do 200.")
+    time.sleep(.5)
+    print("Tak pojď. Hádej.")
+
+def pick():
+    guessesTaken = 0
+    while guessesTaken < 6:
+        time.sleep(.25)
+        enter=input("Hádané číslo: ") 
+        try: 
+            guess = int(enter)
+
+            if guess<=200 and guess>=1: 
+                guessesTaken=guessesTaken+1 
+                if guessesTaken<6:
+                    if guess<number:
+                        print("Číslo, které musíš uhodnout je větší.")
+                    if guess>number:
+                        print("Číslo, které musíš uhodnout je menší.")
+                    if guess != number:
+                        time.sleep(.5)
+                        print("Zkus to znova.")
+                if guess==number:
+                    break 
+            if guess>200 or guess<1:
+                print("Říkal jsem od 0 do 200.")
+                time.sleep(.25)
+                print("Prosím zadej tedy číslo od 0 do 200.")
+
+        except: 
+            print("Nemyslím si že, "+enter+" je číslo.")
+            
+    if guess == number:
+        guessesTaken = str(guessesTaken)
+        print('Dobrá práce, ' + name + '! Uhádl si číslo na ' + guessesTaken + ' pokusů!')
+
+    if guess != number:
+        print('Bohužel číslo, které jsi musel/a uhodnout bylo: ' + str(number))
+
+playagain="yes"
+while playagain=="yes" or playagain=="y" or playagain=="Yes":
+    intro()
+    pick()
+    print("Chceš si zahrát znova?")
+    playagain=input()
